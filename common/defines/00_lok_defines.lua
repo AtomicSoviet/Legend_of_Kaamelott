0ae417ed96a9d5e7fe1a8f8806c3391cd1e8e561
AtomicSoviet <31994582+AtomicSoviet@users.noreply.github.com>
2019-11-15 13:55:11 +0100

    Nouveaux drapeaux plus Kaamelott-esque, et un début d'icons de focus

--- common/defines/00_lok_defines.lua
+++ common/defines/00_lok_defines.lua
@@ -1,2 +1,5 @@
 NDefines.NGame.START_DATE = "460.1.1.12"
 NDefines.NGame.START_DATE = "500.1.1.1"
+
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_WIDTH = 82
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_HEIGHT = 82
