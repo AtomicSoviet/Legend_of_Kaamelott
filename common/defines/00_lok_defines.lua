02faa8da758ff62b08178b5f80787a492ff3fe3a
AtomicSoviet <theatomicsoviet@gmail.com>
2020-02-09 18:08:10 +0100

    Ajouté des villages de pégus

--- common/defines/00_lok_defines.lua
+++ common/defines/00_lok_defines.lua
@@ -1,4 +1,4 @@
-NDefines.NGame.START_DATE = "460.1.1.12"
+NDefines.NGame.START_DATE = "455.1.1.12"
 NDefines.NGame.START_DATE = "500.1.1.1"
 
 NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_WIDTH = 82
